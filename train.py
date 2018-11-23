daa049570a09c6a81106a184e6cf7ee8397fb4ea
William St-Arnaud <william.starnaud@gmail.com>
2018-11-23 17:52:02 -0500

    added training loop to the algorithm. Only does 4x4 block training for now. Will add alpha scaling later

--- train.py
+++ train.py
@@ -5,10 +5,10 @@ import discr
 import util
 
 
-def train(x):
+def train(img):
     """
     Function that generates the network and trains it using the data
-    :param x:
+    :param img: Dataset of images
     :return:
     """
     # define the batch size
@@ -39,6 +39,41 @@ def train(x):
     """
     train_g = util.opt("G/Adam").minimize(-loss)
     train_d = util.opt("D/Adam").minimize(loss)
+
+    """
+    Set up input for training
+    """
+
+    """
+    Train the network
+    """
+    gen_loss = -np.inf
+    discr_loss = np.inf
+    with tf.Session() as sess:
+        while True:
+            for i in range(img.shape[0]):
+                min = batch_size * i
+                max = np.max(min + batch_size, img.shape[0])
+
+                batch = img[min:max, :, :, :]
+
+                # generate images
+                gen_img = sess.run(g)
+
+                # train the generator to fool discriminator
+                gen_loss_ = sess.run(train_g, feed_dict={inp: gen_img})
+
+                # combine the fake and real images
+                discr_inp = np.concatenate((gen_img, batch), axis=0)
+
+                # train the discriminator on the fake images
+                discr_loss_ = sess.run(train_d, feed_dict={inp: discr_inp})
+
+                if np.abs(gen_loss - gen_loss_) < 0.0001 or np.abs(discr_loss - discr_loss_) < 0.0001:
+                    gen_loss, discr_loss = gen_loss_, discr_loss_
+                    break
+                else:
+                    gen_loss, disct_loss = gen_loss_, discr_loss_
 
 
 if __name__ == "__main__":
