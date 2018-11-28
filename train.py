Fixed Nan issues with gradients and included image loading/saving as well

train.py
@@ -3,6 +3,7 @@ import numpy as np
 import gen
 import discr
 import util
+import sys
 
 
 def train(img):
@@ -46,11 +47,7 @@ def train(img):
     """
     Create saver to store the trained weights
     """
-    saver = tf.train.Saver()
-
-    """
-    Set up input for training
-    """
+    saver = tf.train.Saver(tf.get_collection("GEN_VAR"))
 
     """
     Train the network
@@ -63,6 +60,8 @@ def train(img):
 
         step = 1
         while True:
+            g_batch_loss = np.empty([0, ])
+            d_batch_loss = np.empty([0, ])
             for i in np.arange(np.ceil(img.shape[0] / batch_size), dtype=np.int32):
                 min_ = batch_size * i
                 max_ = np.minimum(min_ + batch_size, img.shape[0])
@@ -78,18 +77,29 @@ def train(img):
                 # train the generator to fool discriminator
                 _, gen_loss_ = sess.run([train_g, g_loss_op], feed_dict={fake: gen_img})
 
-                # save checkpoint every 10 steps
-                if step % 10 == 0:
-                    saver.save(sess, "model/model.ckpt", global_step=step)
+                g_batch_loss = np.append(g_batch_loss, gen_loss_)
+                d_batch_loss = np.append(d_batch_loss, discr_loss_)
 
-                if np.abs(gen_loss - gen_loss_) < 0.0001 or np.abs(discr_loss - discr_loss_) < 0.0001:
-                    gen_loss, discr_loss = gen_loss_, discr_loss_
-                    saver.save(sess, "model/trained_model.ckpt")
-                    break
-                else:
-                    gen_loss, disct_loss = gen_loss_, discr_loss_
-                    step += 1
+            print("Step ", step)
+            print("Generator loss is: ", np.mean(g_batch_loss))
+            print("Discriminator loss is: ", np.mean(d_batch_loss), "\n")
+
+            # save checkpoint every 10 steps and print to terminal
+            if step % 10 == 0:
+                saver.save(sess, "model/model.ckpt", global_step=step)
+
+            if np.abs(gen_loss - np.mean(g_batch_loss)) < 0.0001:
+                gen_loss, discr_loss = np.mean(g_batch_loss), np.mean(d_batch_loss)
+                saver.save(sess, "model/trained_model.ckpt")
+                break
+            else:
+                gen_loss, discr_loss = np.mean(g_batch_loss), np.mean(d_batch_loss)
+                step += 1
 
 
 if __name__ == "__main__":
-    train(np.ones([150, 128, 128, 3]))
+    args = sys.argv
+    # Set up input for training
+    data = util.load_img(args[1], [128, 128])
+    data = (data - 127.5) / 127.5
+    train(data)

util.py
@@ -1,4 +1,39 @@
 import tensorflow as tf
+from PIL import Image
+import numpy as np
+import os
+
+
+def load_img(path, size):
+    """
+    Function that loads all images found in path
+    :param path:
+    :param size: size of the input images as a tuple (h, w)
+    :return: All the images as an array of size [n, h, w, 3]
+    """
+    # empty array that will contain all the images
+    h, w = size
+    img = np.empty(shape=[0, h, w, 3], dtype=np.float32)
+    for file in os.listdir(path)[:100]:
+        if file.split(".")[-1] == "jpg":
+            array = np.asarray(Image.open(os.path.join(path, file)))
+            img = np.append(img, [array], axis=0)
+
+    return img
+
+
+def save_img(x, path):
+    """
+    Function that saves all images found in path
+    :param: the images to save as an array of shape [n, h, w, 3]
+    :param path: Path where to save the batch of images
+    :return:
+    """
+    i = 0
+    for el in x:
+        im = Image.fromarray(el.astype("uint8"), "RGB")
+        im.save(path+"/{}.jpg".format(i))
+        i += 1
 
 
 def upsample(x):
@@ -42,7 +77,7 @@ def conv_lay(x, filter_size, num_filters, scope, collection):
     with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
         h, w, in_ch = x.shape[1:]
 
-        init = tf.random_normal_initializer()
+        init = tf.random_normal_initializer(stddev=0.1)
 
         w = tf.get_variable(name="w", shape=filter_size+[in_ch, num_filters],
                             dtype=tf.float32, initializer=init, trainable=True)